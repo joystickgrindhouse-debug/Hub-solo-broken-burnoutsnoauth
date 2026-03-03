26d23f84dd4c8641980d07562b7bf946068d27df
Gregory <joystickgrindhouse@gmail.com>
2026-03-02 17:57:47 -0800

    Update BackgroundShell.jsx

--- src/components/BackgroundShell.jsx
+++ src/components/BackgroundShell.jsx
@@ -2,19 +2,24 @@ import React from "react";
 
 export default function BackgroundShell({ children }) {
   return (
-    <div
-      className="min-h-screen w-full bg-cover bg-center relative"
-      style={{
-        backgroundImage: "url('/background.jpg')"
-      }}
-    >
+    <div className="relative min-h-screen w-full">
+
+      {/* Background Image */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/background.jpg')"
+        }}
+      />
+
       {/* Dark overlay */}
       <div className="absolute inset-0 bg-black/70 backdrop-blur-sm" />
 
-      {/* Content */}
+      {/* Content layer */}
       <div className="relative z-10 min-h-screen flex flex-col">
         {children}
       </div>
+
     </div>
   );
 }
